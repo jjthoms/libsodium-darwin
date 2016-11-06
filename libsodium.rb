bce67186ad04a95d7927e8afaa839b50f36a9e40
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2016-11-06 15:12:41 +0200

    Iterate platform/cpu architecture

--- libsodium.rb
+++ libsodium.rb
@@ -32,22 +32,27 @@ def find_sdks
   sdk_versions = {}
   for line in sdks.lines do
     if line =~ /-sdk iphoneos(\S+)/
-      sdk_versions["ios"]     = $1
+      sdk_versions["iOS"]     = $1
     elsif line =~ /-sdk macosx(\S+)/
-      sdk_versions["macos"]   = $1
+      sdk_versions["macOS"]   = $1
     elsif line =~ /-sdk appletvos(\S+)/
-      sdk_versions["tvos"]    = $1
+      sdk_versions["tvOS"]    = $1
     elsif line =~ /-sdk watchos(\S+)/
-      sdk_versions["watchos"] = $1
+      sdk_versions["watchOS"] = $1
     end
   end
   return sdk_versions
 end
 
 PKG_VER                 = "1.0.11"
+download_and_extract_libsodium()
+
 LIBNAME                 = "libsodium.a"
 VALID_ARHS_PER_PLATFORM = {
-  "iOS" => ["armv7", "armv7s", "arm64", "i386", "x86_64"],
+  "iOS"     => ["armv7", "armv7s", "arm64", "i386", "x86_64"],
+  "macOS"   => ["x86_64"],
+  "tvOS"    => ["arm64","i386", "x86_64"],
+  "watchOS" => ["armv7k","i386", "x86_64"],
 }
 DEVELOPER               = `xcode-select -print-path`
 LIPO                    = `xcrun -sdk iphoneos -find lipo`
@@ -62,10 +67,10 @@ DISTDIR                 = "#{DSTDIR}/libsodium_dist"
 DISTLIBDIR              = "#{DISTDIR}/lib"
 
 sdk_versions            = find_sdks()
-IOS_SDK_VERSION         = sdk_versions["ios"]
-MACOS_SDK_VERSION       = sdk_versions["macos"]
-TVOS_SDK_VERSION        = sdk_versions["tvos"]
-WATCHOS_SDK_VERSION     = sdk_versions["macos"]
+IOS_SDK_VERSION         = sdk_versions["iOS"]
+MACOS_SDK_VERSION       = sdk_versions["macOS"]
+TVOS_SDK_VERSION        = sdk_versions["tvOS"]
+WATCHOS_SDK_VERSION     = sdk_versions["watchOS"]
 
 puts "iOS     SDK version = #{IOS_SDK_VERSION}"
 puts "macOS   SDK version = #{MACOS_SDK_VERSION}"
@@ -74,32 +79,36 @@ puts "tvOS    SDK version = #{TVOS_SDK_VERSION}"
 
 OTHER_CFLAGS            = "-Os -Qunused-arguments"
 
-#download_and_extract_libsodium()
+# Cleanup
+if File.directory? BUILDDIR
+    FileUtils.rm_rf BUILDDIR
+end
+if File.directory? DISTDIR
+    FileUtils.rm_rf DISTDIR
+end
+FileUtils.mkdir_p BUILDDIR
+FileUtils.mkdir_p DISTDIR
+
+# Generate autoconf files
+FileUtils.cd(LIBDIR)
+#exit 1 unless system('./autogen.sh')
+
+PLATFORMS = sdk_versions.keys
+# Compile libsodium for each Apple device platform
+for platform in PLATFORMS
+  # Compile libsodium for each valid Apple device architecture
+  archs = VALID_ARHS_PER_PLATFORM[platform]
+  for arch in archs
+    puts "Building #{platform}/#{arch}..."
+    build_arch_dir="#{BUILDDIR}/#{arch}"
+    FileUtils.mkdir_p(build_arch_dir)
+  end
+end
 
 exit 1
 =begin
-
-
-# Cleanup
-if [ -d $BUILDDIR ]
-then
-    rm -rf $BUILDDIR
-fi
-if [ -d $DISTDIR ]
-then
-    rm -rf $DISTDIR
-fi
-mkdir -p $BUILDDIR $DISTDIR
-
-# Generate autoconf files
-cd ${LIBDIR}; ./autogen.sh
-
-# Iterate over archs and compile static libs
 for ARCH in $ARCHS
 do
-    BUILDARCHDIR="$BUILDDIR/$ARCH"
-    mkdir -p ${BUILDARCHDIR}
-
     case ${ARCH} in
         armv7)
 	    PLATFORM="iPhoneOS"
